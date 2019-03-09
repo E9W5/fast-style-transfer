dc37f834d97825da3a2d79e5b20ba125f505311d
E9W5 <37392997+E9W5@users.noreply.github.com>
2019-03-09 17:28:41 +0100

    Create run_style.sh

---
+++ run_style.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+mkdir data
+cd data
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+mkdir bin
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+unzip train2014.zip
+
+python style.py --style examples/style/cherry-tree-in-bloom-1905.jpg \
+  --checkpoint-dir checkpoint/ \
+  --test examples/content/chicago.jpg \
+  --test-dir examples/content/testimg \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 1000 \
+  --batch-size 20
+
+python evaluate.py --checkpoint checkpoint/model.ckpt \
+  --in-path examples/style/cherry-tree-in-bloom-1905.jpg \
+  --out-path examples/results/
+
