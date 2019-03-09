b7b288015acc09e704fade1878294fe7ea4cb5a3
E9W5 <37392997+E9W5@users.noreply.github.com>
2019-03-09 17:38:12 +0100

    Update run_style.sh

--- run_style.sh
+++ run_style.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
-
+pip install wget
 mkdir data
 cd data
 wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 unzip train2014.zip
+
+cd /.
 
 python style.py --style examples/style/cherry-tree-in-bloom-1905.jpg \
   --checkpoint-dir checkpoint/ \
